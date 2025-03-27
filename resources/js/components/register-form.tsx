77676324340baa9cf4f2abbfe2ef3657eabea135
ibrahimabdouammarat <ibrahimabdouammarat2gmail.com>
2025-03-27 01:35:16 +0100

    feat: register

--- resources/js/components/register-form.tsx
+++ resources/js/components/register-form.tsx
@@ -2,7 +2,7 @@ import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-import { useForm } from '@inertiajs/react';
+import { Link, useForm } from '@inertiajs/react';
 import { LoaderCircle } from 'lucide-react';
 import { FormEventHandler } from 'react';
 import InputError from '@/components/input-error';
@@ -132,9 +132,9 @@ const { data, setData, post, processing, errors, reset } = useForm<Required<Regi
       </div>
       <div className="text-center text-sm">
         Avez-vous déjà un compte?{" "}
-        <a href="#" className="underline underline-offset-4">
+        <Link href="/login" className="underline underline-offset-4">
         Se connecter
-        </a>
+        </Link>
       </div>
     </form>
   )
--- resources/js/pages/auth/register.tsx
+++ resources/js/pages/auth/register.tsx
@@ -22,7 +22,7 @@ export default function RegisterPage() {
       </div>
       <div className="relative hidden bg-muted lg:block">
         <img
-          src="/placeholder.svg"
+          src="/images/login2.webp"
           alt="Image"
           className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
         />
