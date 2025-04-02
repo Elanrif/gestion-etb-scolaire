028b76f7206710d065fb3b52e98903cd6ce821cc
Ammarat22 <ibrahimabdouammarat@gmail.com>
2025-04-02 21:18:15 +0000

    feat:leaout (#8)
    Co-authored-by: ibrahimabdouammarat <ibrahimabdouammarat2gmail.com>

--- resources/js/layouts/app/app-home-layout.tsx
+++ resources/js/layouts/app/app-home-layout.tsx
@@ -1,16 +1,16 @@
 import { AppContent } from '@/components/app-content';
 import { AppShell } from '@/components/app-shell';
-import Footer7 from '@/components/home/footer-7';
-import Navbar1 from '@/components/home/navbar-1';
+import { SiteFooter } from '@/components/site-footer';
+import { SiteHeader } from '@/components/site-header';
 import { type BreadcrumbItem } from '@/types';
 import type { PropsWithChildren } from 'react';
 
 export default function AppHomeLayout({ children }: PropsWithChildren<{ breadcrumbs?: BreadcrumbItem[] }>) {
     return (
         <AppShell>
-            <Navbar1/>
+            <SiteHeader/>
             <AppContent>{children}</AppContent>
-            <Footer7/>
+            <SiteFooter/>
         </AppShell>
     );
 }
