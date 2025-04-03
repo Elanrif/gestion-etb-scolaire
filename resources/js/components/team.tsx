eb0ecfdbafa94331a30a29aadcb910bc2397389d
Elanrif <saidbacoelanrif@gmail.com>
2025-04-03 16:10:18 +0000

    fix: team

--- resources/js/components/team.tsx
+++ resources/js/components/team.tsx
@@ -69,7 +69,7 @@ export default function TeamPage() {
                 {/* Hero Section */}
                 <section className="relative w-full overflow-hidden py-20 md:py-28">
                     <div className="absolute inset-0 z-0">
-                        <img src="/team-hero.jpg" alt="Équipe pédagogique" className="h-full w-full object-cover" />
+                        <img src="/images/pedagogie.jpg" alt="Équipe pédagogique" className="aspect-[16/9] w-full object-cover object-top" />
                         <div className="absolute inset-0 bg-black/70" />
                     </div>
                     <div className="relative z-10 container px-4 md:px-6">
