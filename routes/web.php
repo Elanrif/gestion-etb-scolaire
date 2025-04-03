5924b26c34a490a47473990375e5d369f99fa123
Ammarat22 <ibrahimabdouammarat@gmail.com>
2025-04-03 23:12:20 +0000

    Dashboard index (#24)
    * feat: main
    
    * feat:dashbord
    
    ---------
    
    Co-authored-by: ibrahimabdouammarat <ibrahimabdouammarat2gmail.com>

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
     })->name('dashboard');
 });
 
+require __DIR__.'/admin.php';
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
 require __DIR__.'/home.php';
