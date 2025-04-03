4ad8fa0c598b38f3774015f87c8e4267533e52de
Elanrif <saidbacoelanrif@gmail.com>
2025-04-03 23:40:48 +0000

    feat: doc

--- resources/js/pages/auth/register.tsx
+++ resources/js/pages/auth/register.tsx
@@ -15,7 +15,7 @@ export default function RegisterPage() {
           </a>
         </div>
         <div className="flex flex-1 items-center justify-center">
-          <div className="w-full max-w-md">
+          <div className="w-full max-w-lg">
             <RegisterForm />
           </div>
         </div>
