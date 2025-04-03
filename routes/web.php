45a787388d4c3beedf18a4f269baacb3bbb49a2f
Jawad-coder10 <djawadisaindou@gmail.com>
2025-04-03 22:22:36 +0000

    feat: modification du route home (#22)

--- routes/web.php
+++ routes/web.php
@@ -7,35 +7,6 @@ Route::get('/', function () {
     return Inertia::render('welcome');
 })->name('home');
 
-Route::get('/contact', function () {
-    return Inertia::render('contact');
-})->name('contact');
-
-Route::get('/actuality', function () {
-    return Inertia::render('actuality-page');
-})->name('actuality');
-
-Route::get('/installation', function () {
-    return Inertia::render('installation-page');
-})->name('installation');
-
-Route::get('/formations', function () {
-    return Inertia::render('formations-page');
-})->name('formations');
-
-Route::get('/about', function () {
-    return Inertia::render('about-page');
-})->name('about');
-
-Route::get('/history', function () {
-    return Inertia::render('history-page');
-})->name('history');
-
-Route::get('/team', function () {
-    return Inertia::render('team-page');
-})->name('team');
-
-
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
@@ -44,3 +15,4 @@ Route::middleware(['auth', 'verified'])->group(function () {
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
+require __DIR__.'/home.php';
