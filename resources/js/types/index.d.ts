c265bd70169ded1f15eea6e293065fed630483eb
ibrahimabdouammarat <ibrahimabdouammarat2gmail.com>
2025-04-04 01:20:11 +0000

    feat:student

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -1,6 +1,7 @@
 import { LucideIcon } from 'lucide-react';
 import type { Config } from 'ziggy-js';
 
+
 export interface Auth {
     user: User;
 }
@@ -39,4 +40,22 @@ export interface User {
     created_at: string;
     updated_at: string;
     [key: string]: unknown; // This allows for additional properties...
+}
+
+export interface Student {
+    user: User;
+    first_name: string;
+    last_name: string;
+    gender: string;
+    matricule: number;
+    birthday: Date;
+    level: string;
+    class: string;
+    average: int;
+    number_of_absences: number;
+    email: string;
+    address: string;
+    full_name_parents: string;
+    phone_parents: string;
+    image_url: string;
 }
