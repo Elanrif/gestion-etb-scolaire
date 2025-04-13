d2b923c0f5a25812632e8486d9fb727e4ceebe01
Elanrif <saidbacoelanrif@gmail.com>
2025-04-13 19:31:37 +0100

    feat: dashboard home

---
+++ routes/dashboard.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+
+Route::prefix('dashboard')->name('dashboard.')->group(function () {
+    
+     Route::get('/home', function () {
+        return Inertia::render('dashboard/index');
+    })->name('home');
+
+});
