90d540b28e8feafa98d435edb25fd1e2acaf8033
Jawad-coder10 <djawadisaindou@gmail.com>
2025-05-13 21:59:27 +0100

    fix: translate dashboard in french (#53)

--- resources/js/layouts/settings/layout.tsx
+++ resources/js/layouts/settings/layout.tsx
@@ -13,12 +13,12 @@ const sidebarNavItems: NavItem[] = [
         icon: null,
     },
     {
-        title: 'Password',
+        title: 'Mot de passe',
         href: '/settings/password',
         icon: null,
     },
     {
-        title: 'Appearance',
+        title: 'Apparence ',
         href: '/settings/appearance',
         icon: null,
     },
