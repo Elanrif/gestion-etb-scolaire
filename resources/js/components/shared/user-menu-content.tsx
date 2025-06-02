31291f9b5f07cbb88a36200c61a7c586eae3b43e
ibrahimabdouammarat <ibrahimabdouammarat2gmail.com>
2025-06-02 22:37:48 +0100

    feat: dashboard/link-user

--- resources/js/components/shared/user-menu-content.tsx
+++ resources/js/components/shared/user-menu-content.tsx
@@ -3,7 +3,7 @@ import { UserInfo } from '@/components/shared/user-info';
 import { useMobileNavigation } from '@/hooks/use-mobile-navigation';
 import { type User } from '@/types';
 import { Link } from '@inertiajs/react';
-import { LogOut, Settings } from 'lucide-react';
+import { Home, LogOut, Settings } from 'lucide-react';
 
 interface UserMenuContentProps {
     user: User;
@@ -21,6 +21,12 @@ export function UserMenuContent({ user }: UserMenuContentProps) {
             </DropdownMenuLabel>
             <DropdownMenuSeparator />
             <DropdownMenuGroup>
+                <DropdownMenuItem asChild>
+                    <Link className="block w-full" href={route('dashboard.home')} as="button" prefetch onClick={cleanup}>
+                        <Home className="mr-2" />
+                        Dashboard
+                    </Link>
+                </DropdownMenuItem>
                 <DropdownMenuItem asChild>
                     <Link className="block w-full" href={route('profile.edit')} as="button" prefetch onClick={cleanup}>
                         <Settings className="mr-2" />
